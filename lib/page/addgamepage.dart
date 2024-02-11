9aef75ea1776dce01319b5f7c4b4fc24035e23d2
Marwin Acker <marwin.extern@funk.net>
2024-02-11 22:05:35 +0100

    start implementing modal-based player choosing (this contains the general layout)

--- lib/page/addgamepage.dart
+++ lib/page/addgamepage.dart
@@ -1,26 +1,43 @@
 import 'package:ssr/html.dart' as ssr;
-class GameAddContent implements ssr.Component {
 
+class GameAddContent implements ssr.Component {
   @override
   String render() {
     return """
-<form>
-
-  <div class="grid">
-
-    <label for="team1player1">
-      Team 1 Player 1
-      <input type="text" id="team1player1" name="team1player1" placeholder="Team 1 Player 1" required list="t1p1list">
-    </label>
-  </div>
-  <button type="submit">Submit</button>
-
-  <datalist id="t1p1list">
-    <option value="aa">
-    <option value="bb">
-    <option value="cc">
-  </datalist>
-</form>
+    <div class="container">
+    <h1> add a match </h1>
+    <form>
+      <h2> Team 1 </h2>
+        <div class="grid">
+          <button> choose player 1 </button>
+          <input disabled type="text"/>
+        </div>
+        <div class="grid">
+          <button> choose player 2 </button>
+          <input disabled type="text"/>
+        </div>
+        <h2> Team 2 </h2>
+        <div class="grid">
+          <button> choose player 1 </button>
+          <input disabled type="text"/>
+        </div>
+        <div class="grid">
+          <button> choose player 1 </button>
+          <input disabled type="text"/>
+        </div>
+        <h2> Winner </h2>
+        <div class="grid">
+          <div>
+            <input type="radio" id="t1WinnerId" name="winner" value="team1"/>
+            <label for="t1WinnerId"> Team 1 </label>
+          </div>
+          <div>
+            <input type="radio" id="t2WinnerId" name="winner" value="team2"/>
+            <label for="t2WinnerId"> Team 2 </label>
+          </div>
+        </div>
+    </form>
+    <div>
     """;
   }
 }
